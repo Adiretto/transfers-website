eaba5517939182e11e57f9d3c95413a1b71a0d2a
Adilet <Adilet@Gymme.com`>
2022-07-29 10:09:25 +0600

    frontend

---
+++ app.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $(".question1").on("click", function () {
+        $(".question1-text").slideToggle("fast");
+    });
+    $(".question2").on("click", function () {
+        $(".question2-text").slideToggle("fast");
+    });
+    $(".question3").on("click", function () {
+        $(".question3-text").slideToggle("fast");
+    });
+    $(".question4").on("click", function () {
+        $(".question4-text").slideToggle("fast");
+    });
+    $(".question5").on("click", function () {
+        $(".question5-text").slideToggle("fast");
+    });
+
+});
